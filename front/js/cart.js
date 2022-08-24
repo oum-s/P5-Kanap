f1df3f94be491a62c3ec15b31005e1ef678fb202
oum-s <oumiyouss@hotmail.fr>
2022-08-24 22:21:10 +0200

    essai nouveau code, multiplie par 10 (nombre de produit dans api?)

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,9 +2,32 @@ console.log('salut cart!');
 const Appli = {
   init : function(){
     console.log('hey init');
+    Appli.fetchProducts();
     Appli.displayCart();
     Appli.updateProduct();
     Appli.deleteProduct();
+  },
+
+  fetchProducts : function(){
+    return(
+      fetch(`http://localhost:3000/api/products`)
+      .then(function(res) {
+        // vérifie que la requête s’est bien passée
+        if(res.ok) {
+        //Récupère le résultat de la requête au format json 
+          return res.json();
+        }
+      })
+      .then(function(products) {
+        //  nous le retournons et récupérons sa vraie valeur
+        console.log('ca marche');
+        Appli.totalArticles(products);
+        Appli.priceAmount(products);
+      })
+      .catch(function(err) {
+        console.log(err);
+      })
+    );
   },
 
   displayCart : function(){
@@ -77,10 +100,88 @@ const Appli = {
         console.log(productArray);
       })
     })
-  }
+  },
 
+ /*  resultProducts : function(products){
+    let productArray = JSON.parse(localStorage.getItem('allProduct'));
+    //trouve pas le tableau
+    console.log('productArray',productArray);
+    let finalPrice = 0;
+    let sum = 0;
+    productArray.forEach(theProduct => {
+      
+      products.forEach(product => {
 
+        if ( product._id == theProduct.theId ){
+        console.log('good');
+        
+        let calculPrice = parseInt(product.price);
+        let calculInput = parseInt(theProduct.theQuantity);
+        console.log('calculPrice et input', calculPrice, calculInput);
+        // calcul : prixDuProduit * quantitéDeLinput
+          sum = calculPrice * calculInput;
+          console.log('sum is:', sum);
+
+        // additionner le prix de chaque produit du tableau pour avoir le total
+          
+        };
+      });
+
+    });
+    finalPrice = Number(sum) * Number(productArray.length);
+          console.log('total', finalPrice);
+        //
+          // for (let i = 0; i < productArray.length; i++){ 
+          //     finalPrice = sum[i] ;
+          //     console.log('longueur:',productArray.length);
+          // } 
+
+          let totalPrice = document.getElementById('totalPrice');
+          totalPrice.textContent = finalPrice;
+          console.log('finalPrice', finalPrice);
+  }  */
+  totalArticles : function(products) {
+    let totalItems = 0;
+    let productArray = JSON.parse(localStorage.getItem('allProduct'));
+    for (l in productArray) {
+      // analyser et convertir la valeur 'quantité' dans le localstorage en une chaîne
+      // et renvoie un entier (parseInteger), sur la base décimale de 10
+      const newQuantity = parseInt(productArray[l].theQuantity);
   
-}
+      // attribuer la valeur retournée par parseInt à la variable totalItems
+      totalItems += newQuantity;
+    }
+      // attribuer à #totalQuantité la valeur de totalItems et l'afficher dans le DOM
+      const totalQuantity = document.getElementById('totalQuantity');
+      totalQuantity.textContent = totalItems;
+      console.log('totalItem', totalItems);
+  },
+  
+  // je calcule le montant total du panier
+  priceAmount : function(products) {
+    const calculPrice = [];
+    let productArray = JSON.parse(localStorage.getItem('allProduct'));
+    products.forEach( product => {
+      for (m = 0; m < productArray.length; m++) {
+        // prix de l'article quantité * prix
+        const cartAmount = product.price * productArray[m].theQuantity;
+        console.log('product.price',product.price);
+        console.log('quantité',productArray[m].theQuantity);
+        console.log('cartAmount',cartAmount);
+        calculPrice.push(cartAmount);
+    
+        // la fonction reduce() permet de garder en mémoire les résultats de l'opération
+        // elle fonctionne comme une boucle, avec un accumulateur et la valeur courante
+        const reduce = (previousValue, currentValue) => previousValue + currentValue;
+        total = calculPrice.reduce(reduce);
+      }
+    } );
+    const totalPrice = document.getElementById('totalPrice');
+    totalPrice.textContent = total;
+  }
+  
+  } // fin else : s'il y a des produits dans le panier
+  
+
 // appeler toutes les functions
 document.addEventListener('DOMContentLoaded', Appli.init);
